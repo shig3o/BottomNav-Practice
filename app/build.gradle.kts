2fef307af54c8e8cbad7810832b5383582d3953b
hat <nasharuddinh7@gmail.com>
2024-03-19 21:31:23 +0700

    Fixed grid margin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,7 @@ android {
 }
 
 dependencies {
+    implementation ("com.google.android.material:material:1.4.0")
     implementation ("androidx.cardview:cardview:1.0.0")
     implementation ("com.github.bumptech.glide:glide:4.16.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
